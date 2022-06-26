0f722d0716658bafc1754e5e84bcec81e28887d4
vitorpfranco <vitor.pereira.franco@gmail.com>
2022-06-26 14:29:03 -0300

    Add delete function

--- src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -13,6 +13,8 @@ import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @CrossOrigin(origins="*", maxAge=3600)
@@ -41,5 +43,21 @@ public class ParkingSpotController {
     public ResponseEntity<List<ParkingSpotModel>> getAllParkingSpots(){
         return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findAll());
     }
-
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOneParkingSpot(@PathVariable(value="id") UUID id){
+        Optional<ParkingSpotModel> parkingSpotModelOptional= parkingSpotService.findById(id);
+        if (!parkingSpotModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking Spot not found.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotModelOptional.get());
+    }
+@DeleteMapping("/{id}")
+public ResponseEntity<Object> deleteParkingSpot(@PathVariable(value="id") UUID id){
+    Optional<ParkingSpotModel> parkingSpotModelOptional= parkingSpotService.findById(id);
+    if (!parkingSpotModelOptional.isPresent()){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking Spot not found.");
+    }
+    parkingSpotService.delete(parkingSpotModelOptional.get());
+    return ResponseEntity.status(HttpStatus.OK).body("Parking Spot deleted sucessfully");
+}
 }
